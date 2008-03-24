84bdf03bbe593f0842ade2a7a1a18b55e0a3068f
Bill Shannon <bill.shannon@sun.com>
2008-03-24 16:12:33 -0700

    handle zero length strings specially when quoting - bug 6679333

--- mail/src/main/java/javax/mail/internet/MimeUtility.java
+++ mail/src/main/java/javax/mail/internet/MimeUtility.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -908,6 +908,8 @@ public class MimeUtility {
      */
     public static String quote(String word, String specials) {
 	int len = word.length();
+	if (len == 0)
+	    return "\"\"";	// an empty string is handled specially
 
 	/*
 	 * Look for any "bad" characters, Escape and
