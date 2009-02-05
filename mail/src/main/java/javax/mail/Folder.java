19f6e1d402a1bbff55a911923d0bef9816cbfa06
Bill Shannon <bill.shannon@sun.com>
2009-02-04 17:57:45 -0800

    Remove unused variable.

--- mail/src/main/java/javax/mail/Folder.java
+++ mail/src/main/java/javax/mail/Folder.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -167,11 +167,11 @@ public abstract class Folder {
 	URLName storeURL = getStore().getURLName();
 	String fullname = getFullName();
 	StringBuffer encodedName = new StringBuffer();
-	char separator = getSeparator();
 
 	if (fullname != null) {
 	    /*
 	    // We need to encode each of the folder's names.
+	    char separator = getSeparator();
 	    StringTokenizer tok = new StringTokenizer(
 		fullname, new Character(separator).toString(), true);
 
