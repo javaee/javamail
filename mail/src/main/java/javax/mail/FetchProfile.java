4c800d06638bda385b27bb745a9f07381a6678f0
Bill Shannon <bill.shannon@sun.com>
2009-02-04 17:42:54 -0800

    Override toString method to include item name.

--- mail/src/main/java/javax/mail/FetchProfile.java
+++ mail/src/main/java/javax/mail/FetchProfile.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -135,6 +135,13 @@ public class FetchProfile {
 	 */
 	protected Item(String name) {
 	    this.name = name;
+	}
+
+	/**
+	 * Include the name in the toString return value for debugging.
+	 */
+	public String toString() {
+	    return getClass().getName() + "[" + name + "]";
 	}
     }
 
