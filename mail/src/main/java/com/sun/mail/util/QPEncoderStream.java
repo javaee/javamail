25896bb1140912df4626d092ac94743e9067266e
Bill Shannon <bill.shannon@sun.com>
2009-08-18 20:50:14 -0700

    Ensure that trailing spaces aren't lost at EOF - bug 6872072

--- mail/src/main/java/com/sun/mail/util/QPEncoderStream.java
+++ mail/src/main/java/com/sun/mail/util/QPEncoderStream.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -153,6 +153,10 @@ public class QPEncoderStream extends FilterOutputStream {
      * and closes this output stream
      */
     public void close() throws IOException {
+	if (gotSpace) {
+	    output(' ', true);
+	    gotSpace = false;
+	}
 	out.close();
     }
 
