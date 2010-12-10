f58dca75f6e34384c513187fb3ff6b085e315724
Bill Shannon <bill.shannon@oracle.com>
2010-12-10 15:38:25 -0800

    Improve text/xml DataContentHandle so that it works for JAX-WS as well. Support Source objects as well as String.

--- mail/src/main/java/com/sun/mail/handlers/text_xml.java
+++ mail/src/main/java/com/sun/mail/handlers/text_xml.java
@@ -40,19 +40,118 @@
 
 package com.sun.mail.handlers;
 
+import java.awt.datatransfer.DataFlavor;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import javax.activation.ActivationDataFlavor;
+import javax.activation.DataContentHandler;
+import javax.activation.DataSource;
+import javax.mail.internet.ContentType;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 /**
  * DataContentHandler for text/xml.
  *
+ * @author Anil Vijendran
+ * @author Bill Shannon
  */
 public class text_xml extends text_plain {
-    private static ActivationDataFlavor myDF = new ActivationDataFlavor(
-	java.lang.String.class,
-	"text/xml",
-	"XML String");
 
-    protected ActivationDataFlavor getDF() {
-	return myDF;
+    private final DataFlavor[] flavors;
+
+    public text_xml() {
+	flavors = new DataFlavor[] {
+	    new ActivationDataFlavor(String.class, "text/xml", "XML String"),
+	    new ActivationDataFlavor(String.class, "application/xml",
+					"XML String"),
+	    new ActivationDataFlavor(StreamSource.class, "text/xml", "XML"),
+	    new ActivationDataFlavor(StreamSource.class, "application/xml",
+					"XML")
+	};
+    }
+
+    /**
+     * Return the DataFlavors for this <code>DataContentHandler</code>.
+     *
+     * @return the DataFlavors
+     */
+    public DataFlavor[] getTransferDataFlavors() { // throws Exception;
+	return flavors;
+    }
+
+    /**
+     * Return the Transfer Data of type DataFlavor from InputStream.
+     *
+     * @param df the DataFlavor
+     * @param ds the InputStream corresponding to the data
+     * @return the constructed Object
+     */
+    public Object getTransferData(DataFlavor df, DataSource ds)
+				throws IOException {
+
+	for (int i = 0; i < flavors.length; i++) {
+	    DataFlavor aFlavor = flavors[i];
+	    if (aFlavor.equals(df)) {
+		if (aFlavor.getRepresentationClass() == String.class)
+		    return super.getContent(ds);
+		else if (aFlavor.getRepresentationClass() == StreamSource.class)
+		    return new StreamSource(ds.getInputStream());
+		else
+		    return null;        // XXX - should never happen
+	    }
+	}
+	return null;
+    }
+
+    /**
+     */
+    public void writeTo(Object obj, String mimeType, OutputStream os)
+				    throws IOException {
+	if (!isXmlType(mimeType))
+	    throw new IOException(
+		"Invalid content type \"" + mimeType + "\" for text/xml DCH");
+	if (obj instanceof String) {
+	    super.writeTo(obj, mimeType, os);
+	    return;
+	}
+	if (!(obj instanceof DataSource || obj instanceof Source)) {
+	     throw new IOException("Invalid Object type = "+obj.getClass()+
+		". XmlDCH can only convert DataSource or Source to XML.");
+	}
+
+	try {
+	    Transformer transformer =
+		TransformerFactory.newInstance().newTransformer();
+	    StreamResult result = new StreamResult(os);
+	    if (obj instanceof DataSource) {
+		// Streaming transform applies only to
+		// javax.xml.transform.StreamSource
+		transformer.transform(
+		    new StreamSource(((DataSource)obj).getInputStream()),
+		    result);
+	    } else {
+		transformer.transform((Source)obj, result);
+	    }
+	} catch (Exception ex) {
+	    throw new IOException(
+		"Unable to run the JAXP transformer on a stream "
+		    + ex.getMessage());
+	}
+    }
+
+    private boolean isXmlType(String type) {
+	try {
+	    ContentType ct = new ContentType(type);
+	    return ct.getSubType().equals("xml") &&
+		    (ct.getPrimaryType().equals("text") ||
+		    ct.getPrimaryType().equals("application"));
+	} catch (Exception ex) {
+	    return false;
+	}
     }
 }
---
+++ mail/src/test/java/com/sun/mail/handlers/TextXmlTest.java
@@ -0,0 +1,145 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common Development
+ * and Distribution License("CDDL") (collectively, the "License").  You
+ * may not use this file except in compliance with the License.  You can
+ * obtain a copy of the License at
+ * https://glassfish.dev.java.net/public/CDDL+GPL_1_1.html
+ * or packager/legal/LICENSE.txt.  See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file at packager/legal/LICENSE.txt.
+ *
+ * GPL Classpath Exception:
+ * Oracle designates this particular file as subject to the "Classpath"
+ * exception as provided by Oracle in the GPL Version 2 section of the License
+ * file that accompanied this code.
+ *
+ * Modifications:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * If you wish your version of this file to be governed by only the CDDL or
+ * only the GPL Version 2, indicate your decision by adding "[Contributor]
+ * elects to include this software in this distribution under the [CDDL or GPL
+ * Version 2] license."  If you don't indicate a single choice of license, a
+ * recipient has the option to distribute your version of this file under
+ * either the CDDL, the GPL Version 2 or to extend the choice of license to
+ * its licensees as provided above.  However, if you add GPL Version 2 code
+ * and therefore, elected the GPL Version 2 license, then the option applies
+ * only if the new code is made subject to such option by the copyright
+ * holder.
+ */
+
+package com.sun.mail.handlers;
+
+import java.io.*;
+import java.util.*;
+import java.awt.datatransfer.DataFlavor;
+import javax.activation.*;
+import javax.mail.*;
+import javax.mail.util.ByteArrayDataSource;
+import javax.xml.transform.stream.*;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test the text/xml DataContentHandler.
+ *
+ * XXX - should test other Source objects in addition to StreamSource.
+ *
+ * @author Bill Shannon
+ */
+
+public class TextXmlTest {
+
+    private static String xml = "<test><foo>bar</foo></test>\n";
+    private static byte[] xmlBytes = xml.getBytes();
+
+    // test InputStream to String
+    @Test
+    public void testStreamToStringTextXml() throws Exception {
+	testStreamToString("text/xml");
+    }
+
+    // test InputStream to String
+    @Test
+    public void testStreamToStringApplicationXml() throws Exception {
+	testStreamToString("application/xml");
+    }
+
+    private static void testStreamToString(String mimeType) throws Exception {
+	DataContentHandler dch = new text_xml();
+	DataFlavor df = new ActivationDataFlavor(String.class, mimeType, "XML");
+	DataSource ds = new ByteArrayDataSource(xmlBytes, mimeType);
+	Object content = dch.getContent(ds);
+	assertEquals(String.class, content.getClass());
+	assertEquals(xml, (String)content);
+	content = dch.getTransferData(df, ds);
+	assertEquals(String.class, content.getClass());
+	assertEquals(xml, (String)content);
+    }
+
+    // test InputStream to StreamSource
+    @Test
+    public void testStreamToSource() throws Exception {
+	DataContentHandler dch = new text_xml();
+	DataFlavor df = new ActivationDataFlavor(StreamSource.class,
+						    "text/xml", "XML stream");
+	DataSource ds = new ByteArrayDataSource(xmlBytes, "text/xml");
+	Object content = dch.getTransferData(df, ds);
+	assertEquals(StreamSource.class, content.getClass());
+	String sc = streamToString(((StreamSource)content).getInputStream());
+	assertEquals(xml, sc);
+    }
+
+    // test String to OutputStream
+    @Test
+    public void testStringToStream() throws Exception {
+	DataContentHandler dch = new text_xml();
+	ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	dch.writeTo(xml, "text/xml", bos);
+	String sc = new String(bos.toByteArray(), "us-ascii");
+	assertEquals(xml, sc);
+    }
+
+    // test StreamSource to OutputStream
+    @Test
+    public void testSourceToStream() throws Exception {
+	DataContentHandler dch = new text_xml();
+	ByteArrayOutputStream bos = new ByteArrayOutputStream();
+	StreamSource ss = new StreamSource(new ByteArrayInputStream(xmlBytes));
+	dch.writeTo(ss, "text/xml", bos);
+	String sc = new String(bos.toByteArray(), "us-ascii");
+	// transformer adds an <?xml> header, so can't check for exact match
+	assertTrue(sc.indexOf(xml.trim()) >= 0);
+    }
+
+    /**
+     * Read a stream into a String.
+     */
+    private static String streamToString(InputStream is) {
+	try {
+	    StringBuilder sb = new StringBuilder();
+	    int c;
+	    while ((c = is.read()) > 0)
+		sb.append((char)c);
+	    return sb.toString();
+	} catch (IOException ex) {
+	    return "";
+	} finally {
+	    try {
+		is.close();
+	    } catch (IOException cex) { }
+	}
+    }
+}
