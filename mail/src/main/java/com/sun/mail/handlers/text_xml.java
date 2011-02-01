ee1c559f472e893799aadedd179cd4ff1c63679b
Bill Shannon <bill.shannon@oracle.com>
2011-02-01 14:36:31 -0800

    Clone DataFlavors array before returning.

--- mail/src/main/java/com/sun/mail/handlers/text_xml.java
+++ mail/src/main/java/com/sun/mail/handlers/text_xml.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -81,7 +81,7 @@ public class text_xml extends text_plain {
      * @return the DataFlavors
      */
     public DataFlavor[] getTransferDataFlavors() { // throws Exception;
-	return flavors;
+	return (DataFlavor[])flavors.clone();
     }
 
     /**
