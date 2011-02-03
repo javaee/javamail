656bed27a817568c69715d52700c64cb0176409a
Bill Shannon <bill.shannon@oracle.com>
2011-02-03 15:05:31 -0800

    Fix synchronization of trustAllHosts and trustedHosts.

--- mail/src/main/java/com/sun/mail/util/MailSSLSocketFactory.java
+++ mail/src/main/java/com/sun/mail/util/MailSSLSocketFactory.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -340,7 +340,7 @@ public class MailSSLSocketFactory extends SSLSocketFactory {
 	 */
 	public void checkClientTrusted(X509Certificate[] certs, String authType)
 					throws CertificateException {
-	    if (!(trustAllHosts || trustedHosts != null))
+	    if (!(isTrustAllHosts() || getTrustedHosts() != null))
 		adapteeTrustManager.checkClientTrusted(certs, authType);
 	}
 
@@ -351,7 +351,7 @@ public class MailSSLSocketFactory extends SSLSocketFactory {
 	public void checkServerTrusted(X509Certificate[] certs, String authType)
 					throws CertificateException {
 
-	    if (!(trustAllHosts || trustedHosts != null))
+	    if (!(isTrustAllHosts() || getTrustedHosts() != null))
 		adapteeTrustManager.checkServerTrusted(certs, authType);
 	}
 
