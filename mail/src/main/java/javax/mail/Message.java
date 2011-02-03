e590dbb0863ee691c735a17dcbcd016fa96384ae
Bill Shannon <bill.shannon@oracle.com>
2011-02-03 14:58:05 -0800

    Make msgnum volatile since it's not consistently synchronized.

--- mail/src/main/java/javax/mail/Message.java
+++ mail/src/main/java/javax/mail/Message.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -86,7 +86,7 @@ public abstract class Message implements Part {
      * The number of this message within its folder, or zero if
      * the message was not retrieved from a folder.
      */
-    protected int msgnum	= 0;
+    protected volatile int msgnum = 0;
 
     /**
      * True if this message has been expunged.
