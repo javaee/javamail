caa8a4f35b8eeb6f0158ade9db30d32d07da572b
Bill Shannon <bill.shannon@oracle.com>
2012-01-18 19:38:18 -0800

    According to SASL-IR, empty initial response needs to be sent as "=".

--- mail/src/main/java/com/sun/mail/smtp/SMTPTransport.java
+++ mail/src/main/java/com/sun/mail/smtp/SMTPTransport.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2011 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -792,7 +792,8 @@ public class SMTPTransport extends Transport {
 		    suspendTracing();
 		}
 		if (ir != null)
-		    resp = simpleCommand("AUTH " + mech + " " + ir);
+		    resp = simpleCommand("AUTH " + mech + " " +
+					    (ir.length() == 0 ? "=" : ir));
 		else
 		    resp = simpleCommand("AUTH " + mech);
 
