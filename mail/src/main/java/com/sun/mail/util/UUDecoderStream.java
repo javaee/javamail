b75605999b043b482b73bdcfdca525d0963d746d
Bill Shannon <bill.shannon@oracle.com>
2012-08-14 15:25:48 -0700

    Properly ignore blank lines when ignoring missing "begin" line. (Patch from joegallo)

--- mail/src/main/java/com/sun/mail/util/UUDecoderStream.java
+++ mail/src/main/java/com/sun/mail/util/UUDecoderStream.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -211,7 +211,7 @@ public class UUDecoderStream extends FilterInputStream {
 		}
 		gotPrefix = true;
 		break;
-	    } else if (ignoreMissingBeginEnd) {
+	    } else if (ignoreMissingBeginEnd && line.length() != 0) {
 		int count = line.charAt(0);
 		count = (count - ' ') & 0x3f;
 		int need = ((count * 8)+5)/6;
