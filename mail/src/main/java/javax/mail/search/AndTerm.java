5fe8eb02546d7a3300af4abf0a7e0fb40f819840
Bill Shannon <bill.shannon@oracle.com>
2012-12-20 15:12:19 -0800

    fix protected fields in final classes in javax.mail.search - bug 5683

--- mail/src/main/java/javax/mail/search/AndTerm.java
+++ mail/src/main/java/javax/mail/search/AndTerm.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -57,7 +57,7 @@ public final class AndTerm extends SearchTerm {
      *
      * @serial
      */
-    protected SearchTerm[] terms;
+    private SearchTerm[] terms;
 
     private static final long serialVersionUID = -3583274505380989582L;
 
