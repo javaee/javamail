088c8c67257ee748003a58291215b0ee716bade4
Bill Shannon <bill.shannon@oracle.com>
2013-02-26 13:29:55 -0800

    NullPointerException when accessing the content of a message/rfc822 attachment - bug 5829

--- mail/src/main/java/com/sun/mail/imap/IMAPNestedMessage.java
+++ mail/src/main/java/com/sun/mail/imap/IMAPNestedMessage.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -122,6 +122,13 @@ public class IMAPNestedMessage extends IMAPMessage {
      */
     protected int getFetchBlockSize() {
 	return msg.getFetchBlockSize();
+    }
+
+    /*
+     * Get the enclosing message's ignoreBodyStructureSize. 
+     */
+    protected boolean ignoreBodyStructureSize() {
+	return msg.ignoreBodyStructureSize();
     }
 
     /*
