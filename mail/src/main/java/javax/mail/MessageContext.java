f14243a412d32f4a3f30b442e13e93bdbf412ca1
Bill Shannon <bill.shannon@oracle.com>
2013-02-26 12:13:32 -0800

    Use new Message.getSession() method, instead of accessing the session directly, allowing a Message subclass to usefully override the getSession method.

--- mail/src/main/java/javax/mail/MessageContext.java
+++ mail/src/main/java/javax/mail/MessageContext.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -117,6 +117,6 @@ public class MessageContext {
      */
     public Session getSession() {
 	Message msg = getMessage();
-	return msg != null ? msg.session : null;
+	return msg != null ? msg.getSession() : null;
     }
 }
