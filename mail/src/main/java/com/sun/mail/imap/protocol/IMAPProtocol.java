de4e24ce483c665a7fbd84e47b43700be1def69e
Bill Shannon <bill.shannon@oracle.com>
2014-09-12 17:28:42 -0700

    check for UIDPLUS capability in copyuid method - bug 6430

--- mail/src/main/java/com/sun/mail/imap/protocol/IMAPProtocol.java
+++ mail/src/main/java/com/sun/mail/imap/protocol/IMAPProtocol.java
@@ -1783,6 +1783,8 @@ public class IMAPProtocol extends Protocol {
 
     public CopyUID copyuid(String msgSequence, String mbox, boolean uid)
 				throws ProtocolException {
+	if (!hasCapability("UIDPLUS")) 
+	    throw new BadCommandException("UIDPLUS not supported");
 	// encode the mbox as per RFC2060
 	mbox = BASE64MailboxEncoder.encode(mbox);
 
