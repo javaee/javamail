4f73502c4548e64dd6686830be83ce7452941f27
Bill Shannon <bill.shannon@oracle.com>
2015-11-12 13:58:01 -0800

    Chain exception in MimePartDataSource.getInputStream.

--- mail/src/main/java/javax/mail/internet/MimePartDataSource.java
+++ mail/src/main/java/javax/mail/internet/MimePartDataSource.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2015 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -112,7 +112,9 @@ public class MimePartDataSource implements DataSource, MessageAware {
 	    throw new FolderClosedIOException(fex.getFolder(),
 						fex.getMessage());
 	} catch (MessagingException mex) {
-	    throw new IOException(mex.getMessage());
+	    IOException ioex = new IOException(mex.getMessage());
+	    ioex.initCause(mex);
+	    throw ioex;
 	}
     }
 
