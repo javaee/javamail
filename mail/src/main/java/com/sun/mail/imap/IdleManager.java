66210145c7f85deb25aabbc4852e6390fad429fd
Bill Shannon <bill.shannon@oracle.com>
2016-08-15 13:23:30 -0700

    Fix typo in comment.

--- mail/src/main/java/com/sun/mail/imap/IdleManager.java
+++ mail/src/main/java/com/sun/mail/imap/IdleManager.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2014-2015 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014-2016 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -346,7 +346,7 @@ public class IdleManager {
 	Set<SelectionKey> selectedKeys = selector.selectedKeys();
 	/*
 	 * XXX - this is simpler, but it can fail with
-	 *	 ConncurentModificationException
+	 *	 ConcurrentModificationException
 	 *
 	for (SelectionKey sk : selectedKeys) {
 	    selectedKeys.remove(sk);	// only process each key once
