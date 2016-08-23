11e5b19507de721a8b84c7e72ddf6c7135c26290
Bill Shannon <bill.shannon@oracle.com>
2016-08-23 12:20:07 -0700

    Fix test to use getCanonicalHostName to match InternetAddress - bug 8403

--- mail/src/test/java/javax/mail/internet/GetLocalAddressTest.java
+++ mail/src/test/java/javax/mail/internet/GetLocalAddressTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010-2016 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -59,7 +59,7 @@ public class GetLocalAddressTest {
     private static String localhost;
     static {
 	try {
-	    localhost = InetAddress.getLocalHost().getHostName();
+	    localhost = InetAddress.getLocalHost().getCanonicalHostName();
 	} catch (UnknownHostException ex) {
 	    localhost = "localhost";
 	}
