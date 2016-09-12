83bbdf0045c244b7148e4b0837f08b2358609244
Bill Shannon <bill.shannon@oracle.com>
2016-09-12 11:56:41 -0700

    support varargs in MailLogger.log

--- mail/src/main/java/com/sun/mail/util/MailLogger.java
+++ mail/src/main/java/com/sun/mail/util/MailLogger.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 2012-2015 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012-2016 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -263,7 +263,7 @@ public final class MailLogger {
      * @param msg the message.
      * @param params the message parameters.
      */
-    public void log(Level level, String msg, Object params[]) {
+    public void log(Level level, String msg, Object... params) {
 	if (debug) {
 	    msg = MessageFormat.format(msg, params);
 	    debugOut(msg);
