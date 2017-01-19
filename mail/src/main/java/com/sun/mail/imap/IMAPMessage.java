a43d55104f271d207dfa149d23cf3437306b6fa0
Bill Shannon <bill.shannon@oracle.com>
2017-01-18 16:27:24 -0800

    fix javadoc complaints

--- mail/src/main/java/com/sun/mail/imap/IMAPMessage.java
+++ mail/src/main/java/com/sun/mail/imap/IMAPMessage.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2016 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2017 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -519,6 +519,8 @@ public class IMAPMessage extends MimeMessage implements ReadableMime {
      * Get the message size as a long. <p>
      *
      * Suitable for messages that might be larger than 2GB.
+     * @return	the message size as a long integer
+     * @exception	MessagingException for failures
      * @since	JavaMail 1.6
      */
     public long getSizeLong() throws MessagingException {
