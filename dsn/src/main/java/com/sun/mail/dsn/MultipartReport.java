c89ee4a2ae5646edee90805da75da02e868025b6
Bill Shannon <bill.shannon@oracle.com>
2018-02-05 15:42:22 -0800

    Fix some generics that were missed.

--- dsn/src/main/java/com/sun/mail/dsn/MultipartReport.java
+++ dsn/src/main/java/com/sun/mail/dsn/MultipartReport.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2017 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2018 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -376,7 +376,7 @@ public class MultipartReport extends MimeMultipart {
     private synchronized void setBodyPart(BodyPart part, int index) 
 				throws MessagingException {
 	if (parts == null)	// XXX - can never happen?
-	    parts = new Vector();
+	    parts = new Vector<BodyPart>();
 
 	if (index < parts.size())
 	    super.removeBodyPart(index);
